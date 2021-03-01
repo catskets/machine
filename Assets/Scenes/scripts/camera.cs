9c4b034eff9d576b231bf8b793ac3f6d4343c792
Leo Current <78365840+milkmoth@users.noreply.github.com>
2021-03-01 05:40:54 -0500

    Initial commit

---
+++ Assets/Scenes/scripts/camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform playerBall; //gives the ball a variable
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(playerBall.position.x, playerBall.position.y, transform.position.z); //constantly takes position of ball
+    }
+}
+
+//basic help from https://www.youtube.com/watch?v=VJjD1Tp1I8U
